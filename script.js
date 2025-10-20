76636e13c1c6740dd7561440d6ff3878774e87d6
Isaac Lal <isaaclal124@gmail.com>
2025-10-20 16:28:54 -0400

    Imported current project from 2023 in new ES2025 standard

--- script.js
+++ script.js
@@ -104,5 +104,133 @@ function handleTimelineUpdate(e) {
 speedBtn.addEventListener("click", changePlaybackSpeed);
 
 function changePlaybackSpeed() {
-    
+    let newPlaybackRate = video.playbackRate + 0.25;
+    if (newPlaybackRate > 2) newPlaybackRate = 0.25;
+
+    video.playbackRate = newPlaybackRate;
+    speedBtn.textContent = `${newPlaybackRate}x`
 }
+
+// Captions
+const captions = video.textTracks[0];
+captions.mode = "hidden";
+
+captionsBtn.addEventListener("click", toggleCaptions);
+
+function toggleCaptions() {
+    const isHidden = captions.mode === "hidden";
+    captions.mode = isHidden ? "showing" : "hidden";
+    videoContainer.classList.toggle("captions", isHidden);
+}
+
+// Duration
+video.addEventListener("loadeddata", () => {
+    totalTimeElem.textContent = formatDuration(video.duration);
+});
+
+video.addEventListener("timeupdate", () => {
+    currentTimeElem.textContent = formatDuration(video.currentTime);
+    const percent = video.currentTime / video.duration;
+    timelineContainer.style.setProperty("--progress-position", percent);
+});
+
+const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+    minimumIntegerDigits: 2,
+})
+
+function formatDuration(time) {
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor(time / 60) % 60;
+    const hours = Math.floor(time / 3600);
+
+    if (hours === 0) {
+        return `${minutes}:${leadingZeroFormatter.format(seconds)}`
+    } else {
+        return `${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`
+    }
+}
+
+function skip(duration) {
+    video.currentTime += duration;
+}
+
+// Volume
+muteBtn.addEventListener("click", toggleMute);
+
+volumeSlider.addEventListener("input", e => {
+    video.volume = e.target.value;
+    video.muted = e.target.value === 0;
+});
+
+function toggleMute() {
+    video.muted = !video.muted;
+}
+
+video.addEventListener("volumechange", () => {
+    volumeSlider.value = video.volume;
+    let volumeLevel;
+
+    if (video.muted || video.volume === 0) {
+        volumeSlider.value = 0;
+        volumeLevel = "muted";
+    } else if (video.volume >= 0.5) {
+        volumeLevel = "high";
+    } else {
+        volumeLevel = "low";
+    }
+
+    videoContainer.dataset.volumeLevel = volumeLevel;
+});
+
+// View Modes
+theaterBtn.addEventListener("click", toggleTheaterMode);
+fullScreenBtn.addEventListener("click", toggleFullScreenMode);
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode);
+
+function toggleTheaterMode() {
+    videoContainer.classList.toggle("theater");
+}
+
+function toggleFullScreenMode() {
+    if (document.fullscreenElement == null) {
+        videoContainer.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+}
+
+function toggleMiniPlayerMode() {
+    if (videoContainer.classList.contains("mini-player")) {
+        document.exitPictureInPicture();
+    } else {
+        video.requestPictureInPicture();
+    }
+}
+
+document.addEventListener("fullscreenchange", () => {
+    videoContainer.classList.toggle("full-screen", document.fullscreenElement);
+});
+
+video.addEventListener("enterpictureinpicture", () => {
+    videoContainer.classList.add("mini-player");
+});
+
+video.addEventListener("leavepictureinpicture", () => {
+    videoContainer.classList.remove("mini-player");
+});
+
+// Play/Pause 
+playPauseBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+
+function togglePlay() {
+    video.paused ? video.play() : video.pause();
+}
+
+video.addEventListener("play", () => {
+    videoContainer.classList.remove("paused");
+});
+
+video.addEventListener("pause", () => {
+    videoContainer.classList.add("paused");
+})
